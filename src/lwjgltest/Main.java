d8df9802ddbadfc5ae19614c5f57599799d3e327
Mikko Kuitunen <mikko.kuitunen@tekla.com>
2015-05-08 10:47:34 +0300

    added idea project

---
+++ src/lwjgltest/Main.java
@@ -0,0 +1,10 @@
+package lwjgltest;
+
+import org.lwjgl.Sys;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("LWJGL Version " + Sys.getVersion() + " is working.");
+    }
+}
